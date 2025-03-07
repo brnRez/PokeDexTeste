c95c043cf669ba97490e9d618d887f0eed30cded
Bruno Maciel <maciel2bruno@gmail.com>
2025-03-07 15:36:42 -0300

    Configuração Inicial

---
+++ PokeDexTeste/src/main/java/com/brunao/PokeDexTeste/PokeDexTesteApplication.java
@@ -0,0 +1,13 @@
+package com.brunao.PokeDexTeste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeDexTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeDexTesteApplication.class, args);
+	}
+
+}
---
+++ PokeDexTeste/src/test/java/com/brunao/PokeDexTeste/PokeDexTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunao.PokeDexTeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeDexTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
